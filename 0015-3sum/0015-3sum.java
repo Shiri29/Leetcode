314c6b4031584758df9b865c02e7cf8305103a52
Shirivanth P <shirivanth2909@gmail.com>
2025-06-29 20:28:25 +0530

    Time: 27 ms (86.25%), Space: 51.1 MB (98.42%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,42 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+    static{
+        for (int i=0;i<500;i++){
+            threeSum(new int[3]);
+        }
+    }
+
+    public  static List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
-        
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+        Arrays.sort(nums);
 
-            int left = i + 1, right = nums.length - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
+        for (int i=0;i<n-2;i++){
+            int x = nums[i];
+            if (x>0) break;
+            if (i>0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int left = i+1;
+            int right = n-1;
+            while (left < right){
+                int total = x + nums[left] + nums[right];
+                if (total == 0){
+                    ans.add(List.of(x,nums[left],nums[right]));
                     left++;
+                    while (nums[left] == nums[left-1] && left < right) left++;
+                   
+                }else if (total < 0){
+                    left++;
+                    
+                }else{
                     right--;
-                } else if (sum < 0) {
-                    left++; // need a larger number
-                } else {
-                    right--; // need a smaller number
+                    
                 }
             }
         }
-
         return ans;
+
+
+
     }
 }
